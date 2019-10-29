1b514331dcf9ef55a1b5a8847a29663341e95e21
Lina Costyuk <lina@MacBook-Air-Lina.local>
2019-10-28 23:00:39 -0300

    add check for invalid input

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Calculator {
     private static Scanner sc;
+
     public static void main(String[] args) {
 
         sc = new Scanner(System.in);
@@ -10,34 +11,40 @@ public class Calculator {
 
         do {
             int enterNumber = readInt();
-            if(operation.equals("")) {
+            if (operation.equals("")) {
                 result = enterNumber;
             }
-            if(operation.equals("+")) {
+            if (operation.equals("+")) {
                 result += enterNumber;
             }
-            if(operation.equals("-")){
+            if (operation.equals("-")) {
                 result -= enterNumber;
             }
-            if (operation.equals("*")){
+            if (operation.equals("*")) {
                 result *= enterNumber;
             }
-            if (operation.equals("/")){
-                 result /= enterNumber;
+            if (operation.equals("/")) {
+                result /= enterNumber;
             }
-           operation = askAndRead("Please choose operation: ");
-              
+            operation = askAndRead("Please choose operation: ");
+
         } while (!(operation.equals("=")));
         System.out.println("Total is: " + result);
     }
 
-    public static String askAndRead(String phrase){
+    public static String askAndRead(String phrase) {
         System.out.print(phrase);
         return sc.next();
     }
 
     public static int readInt() {
-        return Integer.parseInt(askAndRead( "Type your number: "));
-
+        while (true) {
+            try {
+                return Integer.parseInt(askAndRead("Type your number: "));
+            } catch (NumberFormatException e) {
+                System.out.println("not able to parse data, please try again");
+            }
+        }
     }
+
 }
